8d9462c1a0fefa2b13576ea7220f52f38217e479
Lilian-yoli <lilian860919@gmail.com>
2021-03-06 13:19:07 +0800

    adjust Assignment-5

--- Week-2/Assignment-5.js
+++ Week-2/Assignment-5.js
@@ -1,16 +1,15 @@
-ffunction binarySearchPosition(numbers, target) {
+function binarySearchPosition(numbers, target) {
   // your code here
-  let n = Math.floor(numbers.length/2)
+
   let a = 0
-  let b = numbers.length
+  let b = numbers.length - 1
   while ( a <= b ){
+    let n = Math.floor((a + b)/2)
     if (target < numbers[n]){
       b = n - 1
-      n = Math.floor((n - a)/2)
     }
     else if (target > numbers[n]){
       a = n + 1
-      n += Math.floor((b - n)/2)
     }
     else {
       return n
@@ -19,4 +18,4 @@ ffunction binarySearchPosition(numbers, target) {
 
 }
 console.log( binarySearchPosition([1, 2, 5, 6, 7], 1) );// should print 0
-console.log( binarySearchPosition([1, 2, 5, 6, 7], 6) ); // should print 3
+console.log( binarySearchPosition([1, 2, 5, 6, 7], 7) ); // should print 3
